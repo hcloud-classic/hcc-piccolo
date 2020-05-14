7ee5f3ddbfd8b9183d76139af07c02a2c794f213
ish-hcc <ish@innogrid.com>
2020-05-14 10:12:25 +0900

    [Hcloud-MiddleWare] Piccolo main
    init 부터 end 프로세스 로직 구현

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"hcc/piccolo/action/graphql"
+	hccGatewayEnd "hcc/piccolo/end"
+	hccGatewayInit "hcc/piccolo/init"
+	"hcc/piccolo/lib/config"
 	"hcc/piccolo/lib/logger"
+	"net/http"
+	"strconv"
 )
 
+func init() {
+	err := hccGatewayInit.MainInit()
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	defer func() {
@@ -13,4 +25,9 @@ func main() {
 	http.Handle("/graphql", graphql.GraphqlHandler)
 	logger.Logger.Println("Opening server on port " + strconv.Itoa(int(config.HTTP.Port)) + "...")
 	err := http.ListenAndServe(":"+strconv.Itoa(int(config.HTTP.Port)), nil)
+	if err != nil {
+		logger.Logger.Println(err)
+		logger.Logger.Println("Failed to prepare http server!")
+		return
+	}
 }
