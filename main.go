a439a2e54e6cb2037215ab60ae36b958a795cbad
ish <ish@innogrid.com>
2020-08-13 18:57:51 +0900

    Refactor initializing and exiting

--- main.go
+++ main.go
@@ -1,21 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"hcc/piccolo/action/graphql"
-	hccGatewayEnd "hcc/piccolo/end"
-	hccGatewayInit "hcc/piccolo/init"
+	"hcc/piccolo/lib/config"
+	"hcc/piccolo/lib/logger"
+	"hcc/piccolo/lib/syscheck"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func init() {
-	err := hccGatewayInit.MainInit()
+	err := syscheck.CheckRoot()
 	if err != nil {
 		panic(err)
 	}
+
+	err = logger.Init()
+	if err != nil {
+		panic(err)
+	}
+
+	config.Parser()
+}
+
+func end(){
+	logger.End()
 }
 
 func main() {
-	defer func() {
-		hccGatewayEnd.MainEnd()
+	// Catch the exit signal
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+	go func(){
+		<- sigChan
+		end()
+		fmt.Println("Exiting piccolo module...")
+		os.Exit(0)
 	}()
 
 	graphql.Init()
