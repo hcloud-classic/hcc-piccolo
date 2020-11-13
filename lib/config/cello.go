9d705730213c59b304b1411b173fee475bc905c7
ish <ish@innogrid.com>
2020-11-13 11:44:26 +0900

    config: Fix config comments

--- lib/config/cello.go
+++ lib/config/cello.go
@@ -3,7 +3,7 @@ package config
 type cello struct {
 	ServerAddress    string `goconf:"cello:cello_server_address"`     // ServerAddress : IP address of server which installed cello module
 	ServerPort       int64  `goconf:"cello:cello_server_port"`        // ServerPort : Listening port number of cello module
-	RequestTimeoutMs int64  `goconf:"cello:cello_request_timeout_ms"` // RequestTimeoutMs : HTTP timeout for GraphQL request to cello module
+	RequestTimeoutMs int64  `goconf:"cello:cello_request_timeout_ms"` // RequestTimeoutMs : HTTP timeout for gRPC request to cello module
 }
 
 // Cello : cello config structure
