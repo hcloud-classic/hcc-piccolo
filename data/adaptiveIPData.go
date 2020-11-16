4e6a9f975ef2d867dcf4ca8836a719c15c754f48
ish <ish@innogrid.com>
2020-11-16 12:06:49 +0900

    [Hcloud-MiddleWare] Piccolo data format
      - Refactor getting list of AdaptiveIP
      - Add UpdateAdaptiveIPData and DeleteAdaptiveIPData

--- data/adaptiveIPData.go
+++ data/adaptiveIPData.go
@@ -2,6 +2,8 @@ package data
 
 import "hcc/piccolo/model"
 
+// Query
+
 // AdaptiveIPData : Data structure of adaptiveip
 type AdaptiveIPData struct {
 	Data struct {
@@ -9,9 +11,24 @@ type AdaptiveIPData struct {
 	} `json:"data"`
 }
 
-type AdaptiveIPAvailableIPListData struct {
+// ListAdaptiveIPData : Data structure of list_adaptiveip
+type ListAdaptiveIPData struct {
 	Data struct {
-		AdaptiveIPAvailableIPList model.AdaptiveIPAvailableIPList `json:"adaptiveip_available_ip_list"`
+		ListAdaptiveIP []model.AdaptiveIP `json:"list_adaptiveip"`
+	} `json:"data"`
+}
+
+// AllAdaptiveIPData : Data structure of all_adaptiveip
+type AllAdaptiveIPData struct {
+	Data struct {
+		AllAdaptiveIP []model.AdaptiveIP `json:"all_adaptiveip"`
+	} `json:"data"`
+}
+
+// NumAdaptiveIPData : Data structure of num_adaptiveip
+type NumAdaptiveIPData struct {
+	Data struct {
+		NumAdaptiveIP model.AdaptiveIPNum `json:"num_adaptiveip"`
 	} `json:"data"`
 }
 
@@ -19,5 +36,19 @@ type AdaptiveIPAvailableIPListData struct {
 type CreateAdaptiveIPData struct {
 	Data struct {
 		AdaptiveIP model.AdaptiveIP `json:"create_adaptiveip"`
+	} `json:"data"`
+}
+
+// UpdateAdaptiveIPData : Data structure of update_adaptiveip
+type UpdateAdaptiveIPData struct {
+	Data struct {
+		AdaptiveIP model.AdaptiveIP `json:"update_adaptiveip"`
+	} `json:"data"`
+}
+
+// DeleteAdaptiveIPData : Data structure of delete_adaptiveip
+type DeleteAdaptiveIPData struct {
+	Data struct {
+		AdaptiveIP model.AdaptiveIP `json:"delete_adaptiveip"`
 	} `json:"data"`
 }
