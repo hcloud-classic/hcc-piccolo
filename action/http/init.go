31869b807cbebee57ae019d0e1204ed36bef715b
ish <ish@innogrid.com>
2021-02-23 16:36:55 +0900

    subscription: Update time for telegraf query

--- action/http/init.go
+++ action/http/init.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"context"
+	"fmt"
 	"github.com/functionalfoundry/graphqlws"
 	graphqlgo "github.com/graphql-go/graphql"
 	"github.com/graphql-go/handler"
@@ -10,8 +11,55 @@ import (
 	"hcc/piccolo/lib/logger"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
+
+func telegrafSubscriptionQueryTimeChange(query string, newTime string) string {
+	newQuery := ""
+
+	querySlice := strings.Split(query, ",")
+	for i := range querySlice {
+		if strings.Contains(querySlice[i], "time") {
+			querySlice[i] = strings.Replace(querySlice[i], " ", "", -1)
+			querySlice[i] = strings.Replace(querySlice[i], "\t", "", -1)
+			querySlice[i] = strings.Replace(querySlice[i], "\n", "", -1)
+			s := strings.Split(querySlice[i], ":")
+			if len(s) == 2 {
+				querySlice[i] = "time: " + "\"" + newTime + "\""
+			}
+		}
+		newQuery += querySlice[i] + ","
+	}
+
+	return newQuery
+}
+
+func telegrafSubscriptionGetNewTime(dataStr string) string {
+	newTime := ""
+
+	if strings.Contains(dataStr, "values") {
+		dataStr = strings.TrimSpace(dataStr)
+		dataStr = strings.Replace(dataStr, " ", "", -1)
+		dataStr = strings.Replace(dataStr, "\t", "", -1)
+		dataStr = strings.Replace(dataStr, "\n", "", -1)
+		dataStr = strings.Replace(dataStr, "\\", "", -1)
+		dataStr = strings.Replace(dataStr, "\"", "", -1)
+		dataStr = strings.Replace(dataStr, "[", "", -1)
+		dataStr = strings.Replace(dataStr, "]", "", -1)
+		slices := strings.Split(dataStr, ":")
+		length := len(slices)
+		for i := range slices {
+			if strings.Contains(slices[i], "values") && length >= i+2 {
+				s := strings.Split(slices[i+1], ",")
+				newTime = s[0]
+				break
+			}
+		}
+	}
+
+	return newTime
+}
 
 // Init : Initialize GraphQL server
 func Init() {
@@ -34,26 +82,39 @@ func Init() {
 	logger.Logger.Println("Serving GraphQL's subscription websocket requests on /subscriptions")
 
 	go func() {
+		newTime := ""
+
 		for true {
 			// This assumes you have access to the above subscription manager
 			subscriptions := subscriptionManager.Subscriptions()
 
 			for conn := range subscriptions {
 				for _, subscription := range subscriptions[conn] {
+					query := strings.TrimSpace(subscription.Query)
+
+					if strings.HasPrefix(query, "subscription") &&
+						strings.Contains(query, "telegraf") {
+						query = telegrafSubscriptionQueryTimeChange(query, newTime)
+					}
+
 					// Prepare an execution context for running the query
 					ctx := context.Background()
 
 					// Re-execute the subscription query
 					params := graphqlgo.Params{
 						Schema:         graphql.Schema, // The GraphQL schema
-						RequestString:  subscription.Query,
+						RequestString:  query,
 						VariableValues: subscription.Variables,
 						OperationName:  subscription.OperationName,
 						Context:        ctx,
 					}
 					result := graphqlgo.Do(params)
 
-					logger.Logger.Println(result)
+					if strings.HasPrefix(query, "subscription") &&
+						strings.Contains(query, "telegraf") {
+						dataStr := fmt.Sprintf("%v", result.Data)
+						newTime = telegrafSubscriptionGetNewTime(dataStr)
+					}
 
 					// Send query results back to the subscriber at any point
 					data := graphqlws.DataMessagePayload{
