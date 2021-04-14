6a795e834ede95dd135e733680083d019e148f88
ish <ish@innogrid.com>
2021-04-15 02:10:46 +0900

    graphql: Add some missed group_id related fileds

--- action/graphql/type/serverType.go
+++ action/graphql/type/serverType.go
@@ -10,6 +10,9 @@ var ServerType = graphql.NewObject(
 			"uuid": &graphql.Field{
 				Type: graphql.String,
 			},
+			"group_id": &graphql.Field{
+				Type: graphql.Int,
+			},
 			"subnet_uuid": &graphql.Field{
 				Type: graphql.String,
 			},
